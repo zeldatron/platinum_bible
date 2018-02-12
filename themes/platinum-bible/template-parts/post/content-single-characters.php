4076b21fdb5ca32c400e809798f6de318474422a
ZeldaLin <zeldatron@gmail.com>
2018-02-12 00:34:26 -0800

    character

--- themes/platinum-bible/template-parts/post/content-single-characters.php
+++ themes/platinum-bible/template-parts/post/content-single-characters.php
@@ -18,72 +18,79 @@
 				<?php the_content(); ?>
 	        </div>
 	        <div class="col-sm-12 col-md-4 col-lg-3">
+	             <?php the_post_thumbnail(); ?>
 	             <ul class="sidebar accordion">
-	                 <?php if(get_field('character_real_name')) { ?>
-	                 <li><strong>Real Name:</strong> <?php the_field('character_real_name'); ?></li>
-	                 <?php } ?>
-	                 
-	                 <?php if(get_field('character_occupation')) { ?>
-	                 <li><strong>Occupation:</strong> <?php the_field('character_occupation'); ?></li>
-	                 <?php } ?>
-	                 
-	                 <?php if(get_field('character_base')) { ?>
-	                 <li><strong>Base of Operations:</strong> <?php the_field('character_base'); ?></li>
-	                 <?php } ?>
-	                 
-					<?php
-						$universes = get_field('character_universe');
-						if( $universes ): 
-				    ?>
-					    <li class="commas"><strong>Location(s) in Multiverse: </strong> 
-				    <?php
-						    foreach( $universes as $post):  
-						        setup_postdata($post);  
-						        echo '<span>' . get_the_title() . ' ' . get_field('symbol') . '</span>';  
-						    endforeach;  
-						    wp_reset_postdata(); 
-					?>
-					    </li>
-					<?php
-						endif;
-					?>							
+	                 <li>
+	                      <ul class="lines">
+			                 
+			                 <?php if(get_field('character_real_name')) { ?>
+			                 <li><strong>Real Name:</strong> <?php the_field('character_real_name'); ?></li>
+			                 <?php } ?>
+			                 
+			                 <?php if(get_field('character_occupation')) { ?>
+			                 <li><strong>Occupation:</strong> <?php the_field('character_occupation'); ?></li>
+			                 <?php } ?>
+			                 
+			                 <?php if(get_field('character_base')) { ?>
+			                 <li><strong>Base of Operations:</strong> <?php the_field('character_base'); ?></li>
+			                 <?php } ?>
+			                 
+							<?php
+								$universes = get_field('character_universe');
+								if( $universes ): 
+						    ?>
+							    <li class="commas"><strong>Location(s) in Multiverse: </strong> 
+						    <?php
+								    foreach( $universes as $post):  
+								        setup_postdata($post);  
+								        echo '<span>' . get_the_title() . ' ' . get_field('symbol') . '</span>';  
+								    endforeach;  
+								    wp_reset_postdata();
+								     
+							?>
+							    </li>
+							<?php
+								endif;
+							?>							
+							
+							<?php
+								$ip = get_field('associated_ips');
+								if( $ip ): 
+						    ?>
+							    <li class="commas"><strong>Associated IPs: </strong> 
+						    <?php
+								    foreach( $ip as $post):  
+								        setup_postdata($post);  
+								        echo '<span>' . get_the_title() . '</span>';  
+								    endforeach;  
+								    wp_reset_postdata(); 
+							?>
+							    </li>
+							<?php
+								endif;
+							?>
+							
+							<?php
+								$teams = get_field('group_affiliations');
+								if( $teams  ): 
+						    ?>
+							    <li class="commas"><strong>Teams: </strong> 
+						    <?php
+								    foreach( $teams as $post):  
+								        setup_postdata($post);  
+								        echo '<span>' . get_the_title() . '</span>';  
+								    endforeach;  
+								    wp_reset_postdata(); 
+							?>
+							    </li>
+							<?php
+								endif;
+							?>							
+														
+					        </ul>
+			        </li>
 					
-					<?php
-						$ip = get_field('associated_ips');
-						if( $ip ): 
-				    ?>
-					    <li class="commas"><strong>Associated IPs: </strong> 
-				    <?php
-						    foreach( $ip as $post):  
-						        setup_postdata($post);  
-						        echo '<span>' . get_the_title() . '</span>';  
-						    endforeach;  
-						    wp_reset_postdata(); 
-					?>
-					    </li>
-					<?php
-						endif;
-					?>
-					
-					<?php
-						$teams = get_field('group_affiliations');
-						if( $teams  ): 
-				    ?>
-					    <li class="commas"><strong>Teams: </strong> 
-				    <?php
-						    foreach( $teams as $post):  
-						        setup_postdata($post);  
-						        echo '<span>' . get_the_title() . '</span>';  
-						    endforeach;  
-						    wp_reset_postdata(); 
-					?>
-					    </li>
-					<?php
-						endif;
-					?>							
-												
-					
-			        <?php $relatives = get_field('character_relatives');
+					<?php $relatives = get_field('character_relatives');
 						if( $relatives  ): ?>
 			        
 			        <li>
@@ -101,7 +108,7 @@
 				        </article>
 			        </li>
 			        <?php endif; ?>
-					
+
 			        <?php $associates = get_field('character_associates');
 						if( $associates  ): ?>
 			        
@@ -139,29 +146,38 @@
 				        </article>
 			        </li>
 			        <?php endif; ?>
-					
-					
-	                 
 	             </ul>
-<!--
-	             
-group_affiliations
-
--->
-
 	        </div>
 	    </div>
-    </section
-
-	<?php if( !empty(get_field('character_artwork'))) { ?>
-	<section>
-	    <h1>Artwork</h1>
+    </section>
+	
 		<?php 
-		$image_ids = get_field('character_artwork', false, false);
-		$shortcode = '[' . 'gallery ids="' . implode(',', $image_ids) . '"]';
-		echo do_shortcode( $shortcode );
-		
-		?>
+		$art = get_field('character_artwork');
+		if( $art ) { ?>
+	
+	<section class="content-block md_grey_bg">
+	    <div class="wrap full-width">
+	    
+	     <header>
+		     <h1>Character Artwork</h1>
+		 </header>
+
+			 <div class="grid">
+				<?php 
+				foreach( $art as $image ): 
+			        $content = '<div class="grid-item md-border-color">';
+			            $content .= '<a class="gallery_image" href="'. $image['url'] .'">';
+			                 $content .= '<img src="'. $image['sizes']['medium_large'] .'" alt="'. $image['alt'] .'" />';
+			            $content .= '</a>';
+			        $content .= '</div>';
+				    if ( function_exists('slb_activate') ){
+				        $content = slb_activate($content);
+				    }  
+					echo $content; 
+				endforeach; ?>
+			 </div>		 
+	    </div>
 	</section>
 	<?php } ?>
+
 </article><!-- #post-## -->
